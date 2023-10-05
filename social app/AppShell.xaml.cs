8bf4cc89e99574a24366c662a373c1fc21556757
asmin <asminmj13@gmail.com>
2023-10-05 17:17:55 +0545

    first

---
+++ social app/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace social_app
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ social app/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+namespace social_app
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Clicked !");
+        }
+    }
+}
